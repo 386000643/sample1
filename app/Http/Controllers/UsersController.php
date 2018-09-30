da769ffec1d5aa5d04a23257a2c3a76b4886c6ec
严丹婵 <386000643@qq.com>
2018-09-30 08:10:53 +0000

    finish user edit,update,index and destroy actions

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,21 @@ use Auth;
 
 class UsersController extends Controller
 {
+    //过滤机制
+    public function __construct(){
+        $this->middleware('auth',[
+            'except'=>['create','show','store','index']
+        ]);
+
+        $this->middleware('guest',[
+            'only'=>['create']
+        ]);
+    }
+
+    public function index(){
+        $users=User::paginate(10);
+        return view('users.index',compact('users'));
+    }
     //用户注册界面
     public function create(){
         return view('users.create');
@@ -35,5 +50,38 @@ class UsersController extends Controller
         Auth::login($user);
         session()->flash('success','注册成功');
         return redirect()->route('users.show',[$user]);
+    }
+
+    //编辑页面
+    public function edit(User $user){
+        $this->authorize('update',$user);
+        return view('users.edit',compact('user'));
+    }
+
+    //更新数据
+    public function update(User $user,Request $request){
+        $this->validate($request,[
+            'name'=>'required|max:50',
+            'password'=>'nullable|min:6|confirmed',
+        ]);
+        $this->authorize('update',$user);
+        $data=[];
+        $data['name']=$request->name;
+        if ($request->password) {
+            $data['password']=bcrypt($request->password);
+
+        }
+        $user->update($data);
+        session()->flash('success','个人资料更新成功');
+        return redirect()->route('users.show',$user->id);
+
+    }
+
+    //删除用户
+    public function destroy(User $user){
+        $this->authorize('destroy',$user);
+        $user->delete();
+        session()->flash('success','成功删除用户');
+        return back();
     }
 }
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //制造数据
+        $users=factory(User::class)->times(50)->make();
+        //插入user表中
+        User::insert($users->makeVisible(['password','remember_token'])->toArray());
+        //对第一个用户进行更新
+        $user=User::find(1);
+        $user->is_admin=true;
+        $user->name='Aufree';
+        $user->email='aufree@yousails.com';
+        $user->password=bcrypt('password');
+        $user->save();
+    }
+}
